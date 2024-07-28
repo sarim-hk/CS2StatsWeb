6bdc1360fdb40f59c70127fdc63f9cf018a3f362
sarim <58489447+sarim-hk@users.noreply.github.com>
2024-07-28 03:25:28 +0100

    add players panel + matches panel

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,19 @@
+import Layout from "./Layout";
 import MatchesPanel from "./MatchesPanel";
+import PlayersPanel from "./PlayersPanel";
 
 function Home() {
-
   return (
-    <div>
-      <MatchesPanel/>
-    </div>
+    <Layout>
+      <div className="flex gap-4">
+        <div className="flex-2">
+          <PlayersPanel />
+        </div>
+        <div className="flex-5">
+          <MatchesPanel />
+        </div>
+      </div>
+    </Layout>
   );
 }
 
