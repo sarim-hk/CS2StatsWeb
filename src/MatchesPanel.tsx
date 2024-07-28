c080dbdf2fdad78acf4cecafb306815a142c77b9
sarim <58489447+sarim-hk@users.noreply.github.com>
2024-07-28 03:25:02 +0100

    nicer styling + consistent padding etc

--- src/MatchesPanel.tsx
+++ src/MatchesPanel.tsx
@@ -23,18 +23,27 @@ function MatchesPanel() {
     }, []);
 
     return (
-        <div className="p-8 bg-gray-700">
+        <div className="p-1 bg-gray-800 font-display divide-y divide-white">
             {matches.map((match: any, index) => (
-                <div key={index} className="bg-gray-400 p-4 mb-4 shadow flex items-start text-white font-display">
+                <div key={index} className="flex items-start p-4 bg-gray-600 ">
 
-                    <div className="flex-1 pl-1 pr-1">{match.MatchDate}</div>
-                    
-                    <div className="items-center">
-                        <div className="flex">
-                            <div className="pl-1 pr-1 flex font-semibold text-blue-700">Counter-Terrorists {match.TeamCTScore}</div>
-                            <div className="pl-1 pr-1 font-semibold text-orange-500">{match.TeamTScore} Terrorists</div>
-                        </div>
-                        <div className="text-xs text-center">{match.Map}</div>
+                    <div className="grid grid-cols-4 flex-1">
+                        <a href="{yourLink1}" className="flex justify-start col-span-1">
+                            <div className="flex items-center justify-center">
+                                {match.MatchDate}
+                            </div>
+                        </a>
+
+                        <a href="{yourLink1}" className="flex justify-end  col-span-3">
+                            <div>
+                                <div className="flex items-center justify-center">
+                                    <div className="pr-1 flex font-semibold text-blue-700">Counter-Terrorists {match.TeamCTScore}</div>
+                                    <div className="pl-1 font-semibold text-orange-500">{match.TeamTScore} Terrorists</div>
+                                </div>
+                                <div className="text-xs text-right">{match.Map}</div>
+                            </div>
+                        </a>
+
                     </div>
                 </div>
             ))}
