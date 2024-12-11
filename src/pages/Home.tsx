65e3db2ed177230577b562c13e51d04cd76826e4
sarim <58489447+sarim-hk@users.noreply.github.com>
2024-12-11 22:27:33 +0000

    fix indent sizes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,21 +4,21 @@ import MatchesPanel from "../components/Match/MatchesPanel";
 import PlayersPanel from "../components/Player/PlayersPanel";
 
 function Home() {
-  return (
-    <Layout>
-      <div className="flex gap-4">
-        <div className="flex-1">
-          <PlayersPanel />
-        </div>
+	return (		
+		<Layout>
+			<div className="flex gap-4">
+				<div className="flex-1">
+					<PlayersPanel />
+				</div>
 
-        <div className="flex-2">
-          <LiveMatchPanel/>
-          <MatchesPanel/>
-        </div>
-        
-      </div>
-    </Layout>
-  );
+				<div className="flex-2">
+					<LiveMatchPanel />
+					<MatchesPanel />
+				</div>
+				
+			</div>
+		</Layout>
+	);
 }
 
 export default Home;
