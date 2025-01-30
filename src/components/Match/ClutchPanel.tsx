27362006101dd4f125354cf9974afe137de0243e
sarim <58489447+sarim-hk@users.noreply.github.com>
2025-01-30 00:57:45 +0000

    add per-team selection

--- src/components/Match/ClutchPanel.tsx
+++ src/components/Match/ClutchPanel.tsx
@@ -17,55 +17,53 @@ function ClutchPanel({ Match, TeamID }: ClutchPanelProps) {
     }
 
     return (
-        <div className="bg-gray-800 p-1">
-            <div className="bg-gray-600 flex flex-col items-start justify-start w-full">
+        <div className="bg-gray-600 flex flex-col items-start justify-start w-full">
 
-                {Object.values(team.Players).map(player => {
-                    const combinedEvents = [
-                        ...Match.Clutches.filter(clutch => clutch.PlayerID === player.PlayerID),
-                        ...Match.Duels.filter(duel => duel.WinnerID === player.PlayerID),
-                    ];
+            {Object.values(team.Players).map(player => {
+                const combinedEvents = [
+                    ...Match.Clutches.filter(clutch => clutch.PlayerID === player.PlayerID),
+                    ...Match.Duels.filter(duel => duel.WinnerID === player.PlayerID),
+                ];
 
-                    const sortedEvents = combinedEvents.sort((a, b) => a.RoundID - b.RoundID);
+                const sortedEvents = combinedEvents.sort((a, b) => a.RoundID - b.RoundID);
 
-                    return (
-                        <div key={player.PlayerID} className="bg-gray-600 p-1 w-full">
-                            
-                            <div className="bg-gray-700 flex items-start w-full p-4">
-                                <div className="flex-shrink-0 flex flex-col items-center justify-center w-16">
-                                    <img src={player.AvatarL} className="w-16 h-16 mb-2" />
-                                    <div className="text-xs text-center">{player.Username}</div>
-                                </div>
-
-                                <div className="flex flex-col ml-4 w-full">
-                                    <div className="flex flex-col w-full">
-
-                                        {sortedEvents.map(event => {
-                                            const isClutch = 'EnemyCount' in event;
-                                            return (
-                                                <div
-                                                    key={isClutch ? `${event.PlayerID}-${event.RoundID}-Clutch` : `${event.WinnerID}-${event.RoundID}-Duel`}
-                                                    className={`py-1 px-2 text-xs mt-0.5 mb-0.5 ${isClutch
-                                                        ? event.Result === 'Win'
-                                                            ? 'bg-green-600 bg-opacity-75'
-                                                            : 'bg-red-600 bg-opacity-50'
-                                                        : 'bg-green-600 bg-opacity-75'
-                                                        }`}
-                                                >
-                                                    Round {event.RoundID - Match.Rounds[0].RoundID + 1}:
-                                                    {isClutch ? ` 1v${event.EnemyCount}` : ' 1v1'}
-                                                </div>
-                                            );
-                                        })}
-
-                                    </div>
-                                </div>
-
+                return (
+                    <div key={player.PlayerID} className="bg-gray-600 p-1 w-full">
+                        
+                        <div className="bg-gray-700 flex items-start w-full p-4">
+                            <div className="flex-shrink-0 flex flex-col items-center justify-center w-16">
+                                <img src={player.AvatarL} className="w-16 h-16 mb-2" />
+                                <div className="text-xs text-center">{player.Username}</div>
                             </div>
+
+                            <div className="flex flex-col ml-4 w-full">
+                                <div className="flex flex-col w-full">
+
+                                    {sortedEvents.map(event => {
+                                        const isClutch = 'EnemyCount' in event;
+                                        return (
+                                            <div
+                                                key={isClutch ? `${event.PlayerID}-${event.RoundID}-Clutch` : `${event.WinnerID}-${event.RoundID}-Duel`}
+                                                className={`py-1 px-2 text-xs mt-0.5 mb-0.5 ${isClutch
+                                                    ? event.Result === 'Win'
+                                                        ? 'bg-green-600 bg-opacity-75'
+                                                        : 'bg-red-600 bg-opacity-50'
+                                                    : 'bg-green-600 bg-opacity-75'
+                                                    }`}
+                                            >
+                                                Round {event.RoundID - Match.Rounds[0].RoundID + 1}:
+                                                {isClutch ? ` 1v${event.EnemyCount}` : ' 1v1'}
+                                            </div>
+                                        );
+                                    })}
+
+                                </div>
+                            </div>
+
                         </div>
-                    );
-                })}
-            </div>
+                    </div>
+                );
+            })}
         </div>
     );
 }
--- src/components/Match/TeamSelectorPanel.tsx
+++ src/components/Match/TeamSelectorPanel.tsx
@@ -1,38 +1,64 @@
-import FullMatchInterface from '../../interfaces/FullMatchInterface';
+import FullMatchInterface from "../../interfaces/FullMatchInterface";
 
 interface TeamSelectorPanelProps {
     Match: FullMatchInterface;
     selectedTeamId: string;
     onTeamSelect: (teamId: string) => void;
+    activePanel: "clutch" | "headtohead";
+    onPanelChange: (panel: "clutch" | "headtohead") => void;
 }
 
-function TeamSelectorPanel({ Match, selectedTeamId, onTeamSelect }: TeamSelectorPanelProps) {
+function TeamSelectorPanel({
+    Match,
+    selectedTeamId,
+    onTeamSelect,
+    activePanel,
+    onPanelChange
+}: TeamSelectorPanelProps) {
     return (
-        <div className="bg-gray-800">
-            <div className="bg-gray-600 p-1">
-                <div className="bg-gray-600 flex flex-col items-start justify-start w-full">
-                    <div className="flex justify-center space-x-2">
-                        
-                        {Object.entries(Match.Teams).map(([teamId, team]) => (
-                            <button
-                                key={teamId}
-                                onClick={() => onTeamSelect(teamId)}
-                                className={`px-3 py-1 text-xs text-white bg-gray-600 
-                                ${selectedTeamId === teamId
-                                        ? team.Side === 2
-                                            ? 'border border-orange-500 ring-1 ring-orange-500'
-                                            : 'border border-blue-500 ring-1 ring-blue-500'
-                                        : 'border border-transparent'}`}
-                            >
-                                {team.TeamName}
-                            </button>
-                        ))}
-                    </div>
-                </div>
+        <div className="bg-gray-700 flex flex-col items-center p-1">
+            <div className="flex justify-center gap-2 p-1">
+                <button
+                    key="clutchButton"
+                    onClick={() => onPanelChange("clutch")}
+                    className={`w-40 py-1 text-xs text-white bg-gray-600 text-center
+                        ${activePanel === "clutch"
+                            ? "border border-green-500 ring-1 ring-green-500"
+                            : "border border-transparent"}`}
+                >
+                    Clutches
+                </button>
+
+                <button
+                    key="headtoheadButton"
+                    onClick={() => onPanelChange("headtohead")}
+                    className={`w-40 py-1 text-xs text-white bg-gray-600 text-center
+                        ${activePanel === "headtohead"
+                            ? "border border-green-500 ring-1 ring-green-500"
+                            : "border border-transparent"}`}
+                >
+                    Head-to-Head
+                </button>
+            </div>
+
+            <div className="flex justify-center gap-2 p-1">
+                {Object.entries(Match.Teams).map(([teamId, team]) => (
+                    <button
+                        key={teamId}
+                        onClick={() => onTeamSelect(teamId)}
+                        className={`w-40 py-1 text-xs text-white bg-gray-600 text-center
+                            ${selectedTeamId === teamId
+                                ? team.Side === 2
+                                    ? "border border-orange-500 ring-1 ring-orange-500"
+                                    : "border border-blue-500 ring-1 ring-blue-500"
+                                : "border border-transparent"}`}
+                    >
+                        {team.TeamName}
+                    </button>
+                ))}
             </div>
         </div>
     );
 }
-
 
 export default TeamSelectorPanel;
